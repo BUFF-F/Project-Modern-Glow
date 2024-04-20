f9fa7a4da5564d8bd88d67ec56cda8fceb8d568a
DuDe4TN <friouia10@gmail.com>
2024-04-20 19:11:35 +0100

    Marciello Final BUFFF

--- App.js
+++ App.js
@@ -9,12 +9,37 @@ app.use(express.static("./Public"));
 app.listen(port, () => {
   console.log(`server start on ${port}`);
 });
-app.get("/", (req, res) => {
+app.get("/Home", (req, res) => {
   res.sendFile(path.join(__dirname, "./Public/sita1.html"));
 });
+app.get("/Yeux", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Public/yeux.html"));
+});
+app.get("/Plus", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Public/plus.html"));
+});
+app.get("/Panier", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Public/Painer.html"));
+});
+app.get("/Levre", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Public/pageteint.html"));
+});
+app.get("/Teint", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Public/pageteint.html"));
+});
+app.get("/Login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Public/login.html"));
+});
+app.get("/Signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Public/inscription.html"));
+});
+app.get("/all", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Public/page erreur.html"));
+});
+
 // app.all("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./Public/sita1.html"));
 // });
 // app.put("/login", (req, res) => {
-   
+
 // });
