450f8e4588cf72040622bd99281c0786804f00a4
DuDe4TN <friouia10@gmail.com>
2024-04-21 13:33:37 +0100

    Aymen Dimanche commit

--- App.js
+++ App.js
@@ -1,45 +1,35 @@
+const Users = require("./UsersDB.JS");
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const Home = require("./Routers/Home.JS");
+const Levre = require("./Routers/Levre.JS");
+const Login = require("./Routers/Login.JS");
+const Panier = require("./Routers/Panier.JS");
+const Plus = require("./Routers/Plus.JS");
+const Signup = require("./Routers/Signup.JS");
+const Teint = require("./Routers/Teint.JS");
+const Yeux = require("./Routers/Yeux.JS");
+
+app.use("/Home", Home);
+app.use("/Levre", Levre);
+app.use("/Login", Login);
+app.use("/Panier", Panier);
+app.use("/Plus", Plus);
+app.use("/Signup", Signup);
+app.use("/Teint", Teint);
+app.use("/Yeux", Yeux);
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(morgan("tiny"));
 app.use(express.static("./Public"));
+
 app.listen(port, () => {
   console.log(`server start on ${port}`);
-});
-app.get("/Home", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Public/sita1.html"));
-});
-app.get("/Yeux", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Public/yeux.html"));
-});
-app.get("/Plus", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Public/plus.html"));
-});
-app.get("/Panier", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Public/Painer.html"));
-});
-app.get("/Levre", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Public/pageteint.html"));
-});
-app.get("/Teint", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Public/pageteint.html"));
-});
-app.get("/Login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Public/login.html"));
-});
-app.get("/Signup", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Public/inscription.html"));
+  console.log(Users);
 });
 app.get("/all", (req, res) => {
   res.sendFile(path.join(__dirname, "./Public/page erreur.html"));
 });
-
-// app.all("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./Public/sita1.html"));
-// });
-// app.put("/login", (req, res) => {
-
-// });
