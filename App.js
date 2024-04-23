7774988e2e44187c5dd0912264f9c89523589353
DuDe4TN <friouia10@gmail.com>
2024-04-23 14:27:25 +0100

    sign up + log in Done

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-const Users = require("./UsersDB.JS");
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
@@ -9,8 +8,10 @@ const Login = require("./Routers/Login.JS");
 const Panier = require("./Routers/Panier.JS");
 const Plus = require("./Routers/Plus.JS");
 const Signup = require("./Routers/Signup.JS");
+const Sign = require("./Routers/Sign.JS");
 const Teint = require("./Routers/Teint.JS");
 const Yeux = require("./Routers/Yeux.JS");
+const Log = require("./Routers/Log.JS");
 const app = express();
 
 app.use("/", Home);
@@ -20,16 +21,18 @@ app.use("/Login", Login);
 app.use("/Panier", Panier);
 app.use("/Plus", Plus);
 app.use("/Signup", Signup);
+app.use("/Sign", Sign);
 app.use("/Teint", Teint);
 app.use("/Yeux", Yeux);
+app.use("/Log", Log);
 
+app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 5000;
 app.use(morgan("tiny"));
 app.use(express.static("./Public"));
 
 app.listen(port, () => {
   console.log(`server start on ${port}`);
-  console.log(Users);
 });
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "./Public/Error.html"));
